951e6e56299a2b0bb962548f6c940fe4ea8a4893
Halim Ashkar <halim.ashkar@cea.fr>
2023-04-13 15:21:31 +0200

    Combining funcitons in ObsevationScheduler for GBM and GW

--- tilepy/include/ObservationScheduler.py
+++ tilepy/include/ObservationScheduler.py
@@ -20,14 +20,25 @@ import json
 
 
 
-def GetSchedule_GW(URL, date,datasetDir,outDir,cfgFile):
+def GetSchedule_confile(URL, date,datasetDir,outDir,cfgFile, Type):
 
-    targetType = 'GW_Pointing'
-    fitsMap, filename = GetGWMap(URL)
+    if Type == 'gbmpng':
+        targetType = 'GBM_Pointing'
+        fitsMap, filename = GetGBMMap(URL)
+        name = URL.split('/')[-3]
+    elif Type == 'gbm':
+        targetType = 'GBM_Pointing'
+        fitsMap = fits.open(URL)
+        filename = URL
+        name = URL.split('all_')[1].split('_v00')[0]
+    else: 
+        targetType = 'GW_Pointing'
+        fitsMap, filename = GetGWMap(URL)
+        name = URL.split('/')[-3]
+
     prob, has3D = Check2Dor3D(fitsMap,filename)
 
-    name = URL.split('/')[-3]
-
+    
     print("===========================================================================================")
     PointingsFile = "False"
     galaxies = datasetDir + "/GLADE.txt"
@@ -115,188 +126,8 @@ def GetSchedule_GW(URL, date,datasetDir,outDir,cfgFile):
             FOLLOWUP = False
             print('No observations are scheduled')
 
-def GetSchedule_GBMfromPNG(URL, date,datasetDir,outDir,cfgFile):
-    targetType = 'GBM_Pointing'
-    fitsMap, filename = GetGBMMap(URL)
-    prob, has3D = Check2Dor3D(fitsMap,filename)
 
-    # filename=args.name
-    name = URL.split('/')[-3]
-
-    PointingsFile = "False"
-    galaxies = datasetDir + "/GLADE.txt"
-    #cfgFile = "./configs/FollowupParameters.ini"
-
-    if has3D:
-
-        ObservationTime = date
-        outputDir = "%s/%s" % (outDir, name)
-        dirName = '%s/PGallinFoV' % outputDir
-
-        if not os.path.exists(dirName):
-            os.makedirs(dirName)
-
-        print("===========================================================================================")
-        print("Starting the LST GBM - 3D pointing calculation with the following parameters\n")
-        print("Filename: ", name)
-        print("Date: ", ObservationTime)
-        print("Previous pointings: ", PointingsFile)
-        print("Catalog: ", galaxies)
-        print("Parameters: ", cfgFile)
-        print("Dataset: ", datasetDir)
-        print("Output: ", outputDir)
-
-        obspar = ObservationParameters()
-        obspar.from_configfile(cfgFile)
-        
-        SuggestedPointings, cat = PGalinFoV(filename, ObservationTime, PointingsFile, galaxies, obspar, dirName)
-
-        if (len(SuggestedPointings) != 0):
-            FOLLOWUP = True
-            outfilename = '%s/SuggestedPointings_GWOptimisation.txt' % dirName
-            ascii.write(SuggestedPointings, outfilename, overwrite=True, fast_writer=False)
-            RankingTimes(ObservationTime, filename, cat, obspar, targetType, dirName,
-                         '%s/SuggestedPointings_GWOptimisation.txt' % dirName, obspar.name)
-            PointingPlotting(prob, obspar, name, dirName,
-                             '%s/SuggestedPointings_GWOptimisation.txt' % dirName, obspar.name, filename)
-        else:
-            FOLLOWUP = False
-            print('No observations are scheduled')
-
-    else:
-        ObservationTime = date
-        outputDir = "%s/%s" % (outDir, name)
-        dirName = '%s/PGWinFoV' % outputDir
-
-        if not os.path.exists(dirName):
-            os.makedirs(dirName)
-
-        print("===========================================================================================")
-        print("Starting the LST GBM - 2D pointing calculation with the following parameters\n")
-        print("Filename: ", name)
-        print("Date: ", ObservationTime)
-        print("Previous pointings: ", PointingsFile)
-        print("Catalog: ", galaxies)
-        print("Parameters: ", cfgFile)
-        print("Dataset: ", datasetDir)
-        print("Output: ", outputDir)
-
-        obspar = ObservationParameters()
-        obspar.from_configfile(cfgFile)
-
-        SuggestedPointings, t0 = PGWinFoV(filename, ObservationTime, PointingsFile, obspar, dirName)
-
-        print(SuggestedPointings)
-        print("===========================================================================================")
-        print()
-
-        if (len(SuggestedPointings) != 0):
-            FOLLOWUP = True
-            outfilename = '%s/SuggestedPointings_GWOptimisation.txt' % dirName
-            ascii.write(SuggestedPointings, outfilename, overwrite=True, fast_writer=False)
-            print()
-            RankingTimes_SkyMapInput_2D(ObservationTime, prob, obspar, targetType, dirName,'%s/SuggestedPointings_GWOptimisation.txt' % dirName, obspar.name)
-            PointingPlotting(prob, obspar, name, dirName, '%s/SuggestedPointings_GWOptimisation.txt' % dirName, obspar.name, filename)
-        else:
-            FOLLOWUP = False
-            print('No observations are scheduled')
-
-def GetSchedule_GBM(URL, date,datasetDir,outDir,cfgFile):
-    targetType = 'GBM_Pointing'
-    fitsMap = fits.open(URL)
-
-    filename = URL
-    prob, has3D = Check2Dor3D(fitsMap,filename)
-
-    # filename=args.name
-    #name = URL.split('/')[-3]
-    name = URL.split('all_')[1].split('_v00')[0]
-
-    PointingsFile = "False"
-    galaxies = datasetDir + "/GLADE.txt"
-    #cfgFile = '/Users/mseglar/Documents/GitLab/lst_gwfollowup/configs/FollowupParameters.ini'
-    #cfgFile = "./configs/FollowupParameters.ini"
-
-    if has3D:
-
-        ObservationTime = date
-        outputDir = "%s/%s" % (outDir, name)
-        dirName = '%s/PGallinFoV' % outputDir
-
-        if not os.path.exists(dirName):
-            os.makedirs(dirName)
-
-        print("===========================================================================================")
-        print("Starting the LST GBM - 3D pointing calculation with the following parameters\n")
-        print("Filename: ", name)
-        print("Date: ", ObservationTime)
-        print("Previous pointings: ", PointingsFile)
-        print("Catalog: ", galaxies)
-        print("Parameters: ", cfgFile)
-        print("Dataset: ", datasetDir)
-        print("Output: ", outputDir)
-
-        obspar = ObservationParameters()
-        obspar.from_configfile(cfgFile)
-
-        SuggestedPointings, cat = PGalinFoV(filename, ObservationTime, PointingsFile, galaxies, obspar, dirName)
-
-        if (len(SuggestedPointings) != 0):
-            FOLLOWUP = True
-            obspar = ObservationParameters()
-            obspar.from_configfile(cfgFile)
-            outfilename = '%s/SuggestedPointings_GWOptimisation_%s.txt' % (dirName,obspar.FOV)
-            ascii.write(SuggestedPointings, outfilename, overwrite=True, fast_writer=False)
-            RankingTimes(ObservationTime, filename, cat, obspar, targetType, dirName,outfilename, obspar.name)
-            PointingPlotting(prob, obspar, name, dirName,outfilename, obspar.name, filename)
-        else:
-            FOLLOWUP = False
-            print('No observations are scheduled')
-
-    else:
-        ObservationTime = date
-        outputDir = "%s/%s" % (outDir, name)
-        dirName = '%s/PGWinFoV' % outputDir
-
-        if not os.path.exists(dirName):
-            os.makedirs(dirName)
-
-        print("===========================================================================================")
-        print("Starting the LST GBM - 2D pointing calculation with the following parameters\n")
-        print("Filename: ", name)
-        print("Date: ", ObservationTime)
-        print("Previous pointings: ", PointingsFile)
-        print("Catalog: ", galaxies)
-        print("Parameters: ", cfgFile)
-        print("Dataset: ", datasetDir)
-        print("Output: ", outputDir)
-
-        obspar = ObservationParameters()
-        obspar.from_configfile(cfgFile)
-        
-        SuggestedPointings, t0 = PGWinFoV(filename, ObservationTime, PointingsFile, obspar, dirName)
-
-        print(SuggestedPointings)
-        print("===========================================================================================")
-        print()
-
-        if (len(SuggestedPointings) != 0):
-            FOLLOWUP = True
-            obspar = ObservationParameters()
-            obspar.from_configfile(cfgFile)
-            outfilename = '%s/SuggestedPointings_GWOptimisation_%s.txt' % (dirName,obspar.FOV)
-            ascii.write(SuggestedPointings, outfilename, overwrite=True, fast_writer=False)
-            print()
-            RankingTimes_SkyMapInput_2D(ObservationTime, prob, obspar, targetType, dirName,outfilename, obspar.name)
-            PointingPlotting(prob, obspar, name, dirName, outfilename, obspar.name, filename)
-        else:
-            FOLLOWUP = False
-            print('No observations are scheduled')
-
-
-
-
-def GetSchedule_GW_AstroCOLIBRI(URL, date,datasetDir,outDir, name, Lat, Lon, Height, gSunDown, HorizonSun, gMoonDown,
+def GetSchedule_funcarg(Type, URL, date,datasetDir,outDir, name, Lat, Lon, Height, gSunDown, HorizonSun, gMoonDown,
                  HorizonMoon, gMoonGrey, gMoonPhase, MoonSourceSeparation,
                  MaxMoonSourceSeparation, max_zenith, FOV, MaxRuns, MaxNights,
                  Duration, MinDuration, UseGreytime, MinSlewing, online,
@@ -304,11 +135,21 @@ def GetSchedule_GW_AstroCOLIBRI(URL, date,datasetDir,outDir, name, Lat, Lon, Hei
                  FulFillReq_Percentage, PercentCoverage, ReducedNside, HRnside,
                  Mangrove):
 
-    targetType = 'GW_Pointing'
-    fitsMap, filename = GetGWMap(URL)
-    prob, has3D = Check2Dor3D(fitsMap,filename)
+    if Type == 'gbmpng':
+        targetType = 'GBM_Pointing'
+        fitsMap, filename = GetGBMMap(URL)
+        name = URL.split('/')[-3]
+    elif Type == 'gbm':
+        targetType = 'GBM_Pointing'
+        fitsMap = fits.open(URL)
+        filename = URL
+        name = URL.split('all_')[1].split('_v00')[0]
+    else: 
+        targetType = 'GW_Pointing'
+        fitsMap, filename = GetGWMap(URL)
+        name = URL.split('/')[-3]
 
-    name = URL.split('/')[-3]
+    prob, has3D = Check2Dor3D(fitsMap,filename) 
 
     print("===========================================================================================")
     PointingsFile = "False"
